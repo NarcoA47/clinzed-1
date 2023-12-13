2e666249c0a610bfdb4bee710797a3e950bddf13
simon cheche <128588090+Cheche17@users.noreply.github.com>
2023-12-13 10:15:32 +0200

    Delete clean directory

--- clean/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from. import views
-urlpatterns = [
-   path('', views.homepage, name="homepage"),
-   path('signup', views.signup, name="signup"),
-   path('signin', views.signin, name="signin"),
-   
-  ]
