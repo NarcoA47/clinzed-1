f1c6dd999ef48476effc46c190b7e943fae68517
GecoGamesStudios <140628063+GecoGamesStudios@users.noreply.github.com>
2023-12-14 19:17:05 +0200

    Legcay dev

--- clinzed-1-main/clean/forms.py
+++
@@ -1,8 +0,0 @@
-
-from django import forms
-from .models import Checkout
-
-class CheckoutForm(forms.ModelForm):
-    class Meta:
-        model = Checkout
-        fields = ['name', 'area', 'pickup_type']
